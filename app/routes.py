bd3b294787909fea5d0a1231a2c149b69068a265
jiewei <jiewei@junyanginfo.com>
2021-09-10 09:36:32 +0800

    update config

--- app/routes.py
+++ app/routes.py
@@ -10,8 +10,10 @@ from app import app
 from flask import render_template, flash, redirect, url_for, request, make_response
 from app.forms import LoginForm
 import requests
-
+from flask_cors import *
 from app.models import School
+
+CORS(app, supports_credentials=True, resources=r"/*")
 
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'
