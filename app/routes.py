2f1d955d8873fff67d8640bbc39014dd541ef2a3
cantorfan@gmail.com <daiby2004>
2021-09-27 19:20:15 +0800

    add read log function

--- app/routes.py
+++ app/routes.py
@@ -7,82 +7,14 @@
 import json
 
 from app import app
-from flask import render_template, flash, redirect, url_for, request, make_response
-from app.forms import LoginForm
-import requests
-from flask_cors import *
-from app.models import School, Course
+import subprocess
 
-CORS(app, supports_credentials=True, resources=r"/*")
-
-headers = {
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'
-}
+log_file_list = ['platform', 'evaluation', 'oms', 'user', 'security', 'subject', 'config', 'eureka',
+                 'zuul', 'volunteer', 'login', 'enterprise', 'content']
 
 
-@app.route('/schools')
-def get_data():  # put application's code here
-
-    page = request.args.get('page', 1, type=int)
-    schools = School.query.order_by(School.id).paginate(
-        page, 10, False)
-    result = {'msg': 'success', 'state': 200, 'pageCount': 23, 'data': []}
-    for school in schools.items:
-        result['data'].append({
-            'school': school.school,
-            'school_img': school.school_img,
-            'school_jianjie': json.loads(school.school_jianjie),
-            'phone': school.phone,
-            'address': school.address,
-            'list_jieshao': json.loads(school.list_jieshao),
-        })
-    return result
-
-
-#     # return result
-
-@app.route('/course')
-def get_course():
-    page = request.args.get('page', 1, type=int)
-    page_number = request.args.get('num', 10, type=int)
-    course_type = request.args.get('course_type', "餐饮", type=str)
-    courses = Course.query.filter_by(course_type=course_type).order_by(Course.id).paginate(
-        page, page_number, False)
-    result = {'msg': 'success', 'state': 200, 'data': []}
-    for course in courses.items:
-        result['data'].append({
-            'name': course.name,
-            'school': course.school,
-            'maxprice': course.maxprice,
-            'minprice': course.minprice,
-            'content': json.loads(course.content),
-            'abstract': json.loads(course.abstract),
-            'address': course.detailAddress,
-            'show_tag': json.loads(course.show_tag),
-            'list_img': course.list_img,
-            'phone': course.phone
-        })
-    return result
-
-
-@app.route('/training')
-def get_training_course_data():
-    page = request.args.get('page', 1, type=int)
-    page_number = request.args.get('num', 10, type=int)
-    courses = Course.query.order_by(Course.id).paginate(
-        page, page_number, False)
-    result = {'msg': 'success', 'state': 200, 'pageCount': 20, 'data': []}
-    for course in courses.items:
-        result['data'].append({
-            'name': course.name,
-            'school': course.school,
-            'maxprice': course.maxprice,
-            'minprice': course.minprice,
-            'content': json.loads(course.content),
-            'abstract': json.loads(course.abstract),
-            'address': course.detailAddress,
-            'show_tag': json.loads(course.show_tag),
-            'list_img': course.list_img,
-            'phone': course.phone
-        })
-    return result
+@app.route('/backend/log/<string:name>')
+def get_data(name):  # put application's code here
+    if name not in log_file_list:
+        return None
+    return subprocess.getoutput("docker logs -f --tail 100 %s" % name)
